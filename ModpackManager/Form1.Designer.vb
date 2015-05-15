5084dc6c8aa43e7a08972da8a623848cca4e92c7
Syths <ARProductives@gmail.com>
2015-05-15 12:39:02 +0200

    Initial Commit
    Adding all existing files

---
+++ ModpackManager/Form1.Designer.vb
@@ -0,0 +1,71 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnDownload = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'btnDownload
+        '
+        Me.btnDownload.Location = New System.Drawing.Point(741, 467)
+        Me.btnDownload.Name = "btnDownload"
+        Me.btnDownload.Size = New System.Drawing.Size(331, 104)
+        Me.btnDownload.TabIndex = 0
+        Me.btnDownload.Text = "Download"
+        Me.btnDownload.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(12, 12)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(523, 1018)
+        Me.ListBox1.TabIndex = 1
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.Location = New System.Drawing.Point(1369, 12)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(523, 1018)
+        Me.ListBox2.TabIndex = 2
+        '
+        'Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1904, 1045)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.btnDownload)
+        Me.Name = "Main"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnDownload As System.Windows.Forms.Button
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents ListBox2 As System.Windows.Forms.ListBox
+
+End Class
---
+++ ModpackManager/Form1.vb
@@ -0,0 +1,88 @@
+﻿Imports MySql.Data.MySqlClient
+Imports System.IO
+Public Class Main
+    Public Sub New()
+        InitializeComponent()
+        Me.StartPosition = FormStartPosition.CenterScreen
+    End Sub
+
+    Private Sub btnDownload_Click(sender As Object, e As EventArgs) Handles btnDownload.Click
+        'My.Computer.Network.DownloadFile("http://download855.mediafire.com/inkmckd604qg/mdx989uyqe0jkeu/EquivalentExchange3-1.7.10-0.3.501.jar", "C:\Test\Mods\EquivalentExchange-1.7.10-0.3.501.jar")
+
+        Dim ds As DataTable = GetTable() 'Datatable with all information from the database
+        Dim files As New List(Of String) 'List containing all the files in the mods folder (excluding mods inside of subfolders e.g. the 1.7.10 folder)
+        Dim dbfiles As New List(Of String) 'List containing all the mods from the database with only their rawname and version stuck together (e.g. "buildcraft" and "1.1.0" --> "buildcraft-1.1.0.jar")
+
+        For i = 0 To ds.Rows.Count - 1 'Add all the mods to the dbfiles (as mentioned above)
+            dbfiles.Add(ds.Rows(i)(4) & "-" & ds.Rows(i)(3) & ".jar")
+        Next
+
+        Dim path As String = "C:\Users\Public\Documents\MultiMC\instances\Itvara Server\minecraft\mods" 'Path to the MultiMC mods folder (should be asked on first startup and stored in registry)
+        Dim dir As New DirectoryInfo(path) 'The actual directory in variable format
+
+        For Each fle In dir.GetFiles("*.jar") 'Getting all the files from the specified mods folder and adding them to their respective list (files)
+            files.Add(fle.Name)
+        Next
+
+        MsgBox(files.Count & " | " & dbfiles.Count) 'Quick count comparison (debug)
+
+        Dim found As Boolean
+
+        For Each item In files
+            Dim i As Integer = 0
+
+            For i = 0 To dbfiles.Count - 1
+                If item.ToString = dbfiles(i).ToString Then
+                    Exit For
+                Else
+                    found = False
+                End If
+            Next
+
+            If Not found Then
+                MsgBox("Error: Mismatch! " & item.ToString & "|" & dbfiles(i).ToString)
+            End If
+        Next
+
+        ListBox1.DataSource = files
+        ListBox2.DataSource = dbfiles
+    End Sub
+
+    Private Function GetTable() As DataTable
+        Dim myConn As New MySqlConnection
+        Dim sqlStr As String
+        Dim ds As New DataSet
+        Dim adp As MySqlDataAdapter
+
+        sqlStr = "select * from Mods_List ORDER BY modname"
+        myConn.ConnectionString = "server=" & My.Resources.Ip & ";User id=" & My.Resources.User & ";password=" & My.Resources.Pass & ";database=" & My.Resources.Db
+
+        Try
+            myConn.Open()
+            adp = New MySqlDataAdapter(sqlStr, myConn)
+            adp.FillSchema(ds, SchemaType.Source, "Mods_List")
+            adp.Fill(ds, "Mods_List")
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+
+        myConn.Close()
+
+        Return ds.Tables(0)
+
+        myConn.Dispose()
+        adp.Dispose()
+        ds.Dispose()
+    End Function
+
+    Private Function NewRecord(ByVal modname As String, ByVal version As String, ByVal filename As String, ByVal download_url As String, ByVal website_url As String)
+        Dim cmdInsert As New MySqlCommand
+
+        cmdInsert.Parameters.AddWithValue("@value1", modname)
+        cmdInsert.Parameters.AddWithValue("@value2", version)
+        cmdInsert.Parameters.AddWithValue("@value3", filename)
+        cmdInsert.Parameters.AddWithValue("@Value4", download_url)
+
+        Dim sqlStr As String = "INSERT INTO Mods_List (modname, version, filename, download_url"
+    End Function
+End Class
